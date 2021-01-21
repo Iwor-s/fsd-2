91775d3c5a33a009d30c0a4043fea865ff577bfb
Iw <iwor@list.ru>
2021-01-21 14:17:40 +0300

    Added Webpack babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,18 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							'@babel/preset-env'
+						]
+					}
+				}
+			},
+			{
 				test: /\.css$/,
 				use: css()
 			},
